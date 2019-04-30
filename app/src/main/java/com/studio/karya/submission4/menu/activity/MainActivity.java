00b65c6fab97eb2786ee0c5d829b074f7ee0dda2
syahferiaswan <syahferiaswan@gmail.com>
2019-04-30 17:44:29 +0700

    atur searchview

--- app/src/main/java/com/studio/karya/submission4/menu/activity/MainActivity.java
+++ app/src/main/java/com/studio/karya/submission4/menu/activity/MainActivity.java
@@ -3,13 +3,16 @@ package com.studio.karya.submission4.menu.activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.provider.Settings;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 import androidx.fragment.app.Fragment;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -19,7 +22,9 @@ import com.studio.karya.submission4.menu.fragment.fav.FavContainerFragment;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = MainActivity.class.getSimpleName();
     BottomNavigationView menuBottom;
+    int position_menu = 0;
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
@@ -42,12 +47,15 @@ public class MainActivity extends AppCompatActivity {
                 Fragment fragment;
                 switch (menuItem.getItemId()) {
                     case R.id.action_movie:
+                        position_menu = 0;
                         bundle.putString(ContentFragment.TYPE, "movie");
                         fragment = new ContentFragment();
                         fragment.setArguments(bundle);
                         loadFragment(fragment);
+
                         break;
                     case R.id.action_tv:
+                        position_menu = 1;
                         bundle.putString(ContentFragment.TYPE, "tv");
                         fragment = new ContentFragment();
                         fragment.setArguments(bundle);
@@ -75,18 +83,37 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater menuInflater = getMenuInflater();
         menuInflater.inflate(R.menu.menu_toolbar, menu);
+
         return super.onCreateOptionsMenu(menu);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()){
+        switch (item.getItemId()) {
             case R.id.action_language:
                 Intent intent = new Intent(Settings.ACTION_LOCALE_SETTINGS);
                 startActivityForResult(intent, 101);
                 break;
             case R.id.action_search:
 
+                //TODO with position menu, can filter with searchview with diferent fragment
+
+                SearchView searchView = (SearchView) item.getActionView();
+                searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+                    @Override
+                    public boolean onQueryTextSubmit(String query) {
+                        //call with api
+                        Log.d(TAG, query+" submit");
+                        return false;
+                    }
+
+                    @Override
+                    public boolean onQueryTextChange(String newText) {
+                        //call with adapter
+                        Log.d(TAG, newText);
+                        return false;
+                    }
+                });
                 break;
         }
         return super.onOptionsItemSelected(item);
