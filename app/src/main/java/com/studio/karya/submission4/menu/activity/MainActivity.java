4e83a4cb2f1e2fd71d7879721640b03c8366bdfa
syahferiaswan <syahferiaswan@gmail.com>
2019-05-01 13:54:40 +0700

    test 2

--- app/src/main/java/com/studio/karya/submission4/menu/activity/MainActivity.java
+++ app/src/main/java/com/studio/karya/submission4/menu/activity/MainActivity.java
@@ -97,7 +97,7 @@ public class MainActivity extends AppCompatActivity {
             case R.id.action_search:
 
                 //TODO with position menu, can filter with searchview with diferent fragment
-                //check
+                //check 2
 
                 SearchView searchView = (SearchView) item.getActionView();
                 searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
